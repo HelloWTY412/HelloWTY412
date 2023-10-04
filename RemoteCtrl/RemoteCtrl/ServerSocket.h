da14099593420fde79f3257614084b97385d21e5
HelloWTY412 <w2865513797@gmail.com>
2023-10-04 21:42:41 +0800

    完成客户端类图的绘制

--- RemoteCtrl/RemoteCtrl/ServerSocket.h
+++ RemoteCtrl/RemoteCtrl/ServerSocket.h
@@ -119,24 +119,6 @@ protected:
 		return send(m_client, pack.Data(), pack.Size(), 0) > 0;
 	}
 
-	bool GetFilePath(string& strPath) {
-		if ((m_packet.sCmd >= 2) && (m_packet.sCmd <=4)||(m_packet.sCmd ==9)) {
-			strPath = m_packet.strData;
-			return TRUE;
-		}
-		return FALSE;
-	};
-
-	bool GetMouseEvent(MOUSEEV &mouse) {
-		if (m_packet.sCmd == 5) {
-			memcpy(&mouse, m_packet.strData.c_str(), sizeof(MOUSEEV));
-			return TRUE;
-		}
-		return FALSE;
-	};
-	CPacket& GetPacket() {
-		return m_packet;
-	}
 	void CloseClient() {
 		if (m_client != INVALID_SOCKET) {
 			closesocket(m_client);
